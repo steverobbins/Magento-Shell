4cfddab5ee3ae72ce4427ca4fea608d31a1790f3
Steve Robbins <steven.j.robbins@gmail.com>
2015-04-16 18:21:07 -0700

    Rewrite with a Zend log formatter

---
+++ src/lib/Local/Shell/Formatter.php
@@ -0,0 +1,68 @@
+<?php
+
+class Local_Shell_Formatter extends Zend_Log_Formatter_Abstract
+{
+    /**
+     * Factory for SD_ShopTo_Log_Formatter class
+     *
+     * @param  array|Zend_Config $options
+     * @return SD_ShopTo_Log_Formatter
+     */
+    public static function factory($options)
+    {
+        return $this;
+    }
+
+    /**
+     * Formats data into a single line to be written by the writer.
+     *
+     * @param  array  $event
+     * @return string
+     */
+    public function format($event)
+    {
+        return $this->color($event['timestamp'])->dark_gray()
+            . str_repeat(' ', 7 - strlen($event['priorityName']))
+            . '[' . $this->priorityColor($event['priorityName'], $event['priority']) . '] '
+            . $event['message'] . PHP_EOL;
+    }
+
+    /**
+     * Colorize by zend log level
+     *
+     * @param  string  $message
+     * @param  integer $priority
+     * @return Colors_Color
+     */
+    public function priorityColor($message, $priority)
+    {
+        switch ($priority) {
+            case Zend_Log::INFO:
+                return $this->color($message)->green();
+            case Zend_Log::NOTICE:
+                return $this->color($message)->cyan();
+            case Zend_Log::WARN:
+                return $this->color($message)->yellow();
+            case Zend_Log::ERR:
+                return $this->color($message)->red();
+            case Zend_Log::CRIT:
+                return $this->color($message)->red()->bold();
+            case Zend_Log::ALERT:
+                return $this->color($message)->white()->bg_red();
+            case Zend_Log::EMERG:
+                return $this->color($message)->white()->bg_red()->bold()->blink();
+        }
+        return $this->color($message)->light_green();
+    }
+
+    /**
+     * Color a string
+     *
+     * @param  string $message
+     * @return Colors_Color
+     */
+    public function color($message)
+    {
+        return new Colors_Color($message);
+    }
+}
---
+++ src/shell/local/sample.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once 'abstract.php';
+
+class Local_Shell_SampleScript extends Local_Shell_Abstract
+{
+    /**
+     * Do the thing
+     * 
+     * @return void
+     */
+    public function run()
+    {
+        $this->log->info('Script execution has started');
+        $products = $this->getFakeProductCollection();
+        $count = count($products);
+        $this->log->debug('Found ' . $count . ' product(s) to process');
+        $bar = $this->progressBar($count);
+        $i = 0;
+        foreach ($products as $product) {
+            $bar->update(++$i);
+            // $product->setDescription(Mage::helper('core')->stripTags($product->getDescription()));
+            if (mt_rand(0, 19) > 1) { // try
+                // $product->save();
+            } else { // catch                
+                $this->log->err('An error occured while saving product ' . $product['id']);
+            }
+            sleep(1);
+        }
+        $bar->finish();
+        $this->log->info('Complete');
+    }
+
+    /**
+     * Pretend we're getting some products
+     * 
+     * @return array
+     */
+    public function getFakeProductCollection()
+    {
+        $products = array();
+        for ($i = 1; $i < 21; $i++) {
+            $products[] = new Varien_Object(array(
+                'id'  => $i
+            ));
+        }
+        return $products;
+    }
+}
+
+$shell = new Local_Shell_SampleScript();
+$shell->run();
